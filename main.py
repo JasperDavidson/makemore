87cd39e2317ec9b3a8c2d5c89d7e9b1cfdfe5894
JasperDavidson <jasperdavdsn@gmail.com>
2026-07-11 19:38:24 -0700

    model smoothing + log likelihood loss

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import torch
 
 
+def norm_log_likelihood_loss(
+    words: list[str], stoi: dict[str, int], bigram_probs: torch.Tensor
+) -> torch.Tensor:
+    log_likelihood = torch.Tensor([0.0])
+    n_likelihoods = 0
+
+    for word in words:
+        chrs = ["."] + list(word) + ["."]
+        for ch1, ch2 in zip(chrs, chrs[1:]):
+            ch1_idx = stoi[ch1]
+            ch2_idx = stoi[ch2]
+
+            n_likelihoods += 1
+            log_likelihood += torch.log(bigram_probs[ch1_idx][ch2_idx])
+
+    return -(log_likelihood / n_likelihoods)
+
+
 def main():
+    # Word dataset to train off of
     words = open("names.txt", "r").read().splitlines()
 
-    char_probs = torch.zeros((27, 27))
+    # Set up probability tensor conversion arrays for indexing/retrieval
+    char_probs = torch.ones((27, 27))  # Model smoothing = 1
     chars = ["."] + sorted(list(set("".join(words))))
     stoi = {s: i for i, s in enumerate(chars)}
     itos = {i: s for i, s in enumerate(chars)}
 
+    # Count the number of occurences of every bigram
     for word in words:
         chrs = ["."] + list(word) + ["."]
         for ch1, ch2 in zip(chrs, chrs[1:]):
@@ -17,8 +38,12 @@ def main():
 
             char_probs[ch1_idx][ch2_idx] += 1
 
-    normalized_char_probs = torch.nn.functional.normalize(char_probs, p=1, dim=1)
+    # Normalize the bigram rows + calculate the negative normalized log likelihood loss
+    torch.nn.functional.normalize(char_probs, p=1, dim=1, out=char_probs)
+    loss = norm_log_likelihood_loss(words, stoi, char_probs)
+    print("Log likelihood loss = ", loss)
 
+    # Given a random seed, sample from the bigram probabilites to form a new word
     new_word = []
     prev_char_idx = 0
     word_gen = torch.Generator()
@@ -26,9 +51,7 @@ def main():
 
     while True:
         prev_char_idx = int(
-            torch.multinomial(
-                normalized_char_probs[prev_char_idx], 1, generator=word_gen
-            ).item()
+            torch.multinomial(char_probs[prev_char_idx], 1, generator=word_gen).item()
         )
         prev_char = itos[prev_char_idx]
 
