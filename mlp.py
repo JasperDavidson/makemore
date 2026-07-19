23e5a9daf89d3611cf2c3b27f74557f4cd1d1beb
Jasper Davidson <jasperdavdsn@gmail.com>
2026-07-18 19:11:23 -0700

    Added forward pass

--- mlp.py
+++ mlp.py
@@ -1,6 +1,34 @@
 import torch
 from helpers import find_mappings
 
+block_size = 3
+embedding_dims = 2
+l1_size = 100
+l_out_size = 27  # Matches number of character options
+
+
+def forward_pass(embeddings: torch.Tensor) -> torch.Tensor:
+    # Run the MLP
+    # Layer One
+    w1 = torch.randn(block_size * embedding_dims, l1_size)
+    b1 = torch.randn(l1_size)
+
+    # Perform layer one calculation
+    # - Note that we resize to (-1, 6) in order to pass all block_size * embedding_dims vals to the first layer
+    # - tanh introduces non-linearity into the system, addressing the flaw of the simple 27-neuron layer by allowing the network to learn features
+    l1_out = torch.tanh(embeddings.view(-1, 6) @ w1 + b1)
+
+    # Output layer
+    w_out = torch.randn(l1_size, l_out_size)
+    b_out = torch.randn(l_out_size)
+
+    # Compute the output and apply softmax
+    mlp_out = l1_out @ w_out + b_out
+    mlp_exp = mlp_out.exp()
+    prob = mlp_exp / (mlp_exp.sum(1, keepdim=True))
+
+    return prob
+
 
 def train_mlp():
     # Word dataset to train off of
@@ -9,7 +37,6 @@ def train_mlp():
     itos, stoi = find_mappings(chars, ".")
 
     # Build training and validation datasets
-    block_size = 3
     training, validation = [], []
 
     for word in words[:5]:
@@ -24,6 +51,8 @@ def train_mlp():
     training = torch.tensor(training)
     validation = torch.tensor(validation)
     embedding_space = torch.randn(
-        27, 2
+        27, embedding_dims
     )  # Randomly initialize the embeddings, learned via gradient optimization
     cur_embeddings = embedding_space[training]
+
+    forward_pass(cur_embeddings)
