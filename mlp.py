d014921ed47e275f0399a8a890320712b9de9f92
Jasper Davidson <jasperdavdsn@gmail.com>
2026-07-18 20:37:41 -0700

    Small corrections + cleanup
    Squashed commits:
    - Consolidating parameter grad/data/parameter setting
    - Correctly training embeddings + switch to torch cross entropy

--- mlp.py
+++ mlp.py
@@ -12,47 +12,47 @@ l1_size = 100
 l_out_size = 27  # Matches number of character options
 
 
-def forward_pass(embeddings: torch.Tensor, validation: torch.Tensor, train_iter: int):
+def forward_pass(training: torch.Tensor, validation: torch.Tensor, train_iter: int):
+    embedding_space = torch.randn(
+        27, embedding_dims
+    )  # Randomly initialize the embeddings, learned via gradient optimization
+
     # Layer One
-    w1 = torch.randn(block_size * embedding_dims, l1_size, requires_grad=True)
-    b1 = torch.randn(l1_size, requires_grad=True)
+    w1 = torch.randn(block_size * embedding_dims, l1_size)
+    b1 = torch.randn(l1_size)
 
     # Output layer
-    w_out = torch.randn(l1_size, l_out_size, requires_grad=True)
-    b_out = torch.randn(l_out_size, requires_grad=True)
+    w_out = torch.randn(l1_size, l_out_size)
+    b_out = torch.randn(l_out_size)
+
+    parameters = [embedding_space, w1, b1, w_out, b_out]
+    for p in parameters:
+        p.requires_grad = True
 
     for cur_iter in range(train_iter):
+        cur_embeddings = embedding_space[training]
+
         # Perform layer one calculation
         # - Note that we resize to (-1, 6) in order to pass all block_size * embedding_dims vals to the first layer
         # - tanh introduces non-linearity into the system, addressing the flaw of the simple 27-neuron layer by allowing the network to learn features
-        l1_out = torch.tanh(embeddings.view(-1, 6) @ w1 + b1)
+        l1_out = torch.tanh(cur_embeddings.view(-1, 6) @ w1 + b1)
 
-        # Compute the output and apply softmax
-        mlp_out = l1_out @ w_out + b_out
-        mlp_exp = mlp_out.exp()
-        prob = mlp_exp / (mlp_exp.sum(1, keepdim=True))
-
-        data_loss = -prob[torch.arange(len(validation)), validation].log().mean()
+        # Compute the output and loss
+        logits = l1_out @ w_out + b_out
+        data_loss = torch.nn.functional.cross_entropy(logits, validation)
         reg_loss = (w1**2).sum().mean() + (w_out**2).sum().mean()
         loss = data_loss + reg_loss_param * reg_loss
         print(f"Data loss @ {cur_iter} = {data_loss}")
         print(f"Reg loss @ {cur_iter} = {reg_loss}")
         print(f"Loss @ {cur_iter} = {loss}")
-        w1.grad = None
-        b1.grad = None
-        w_out.grad = None
-        b_out.grad = None
 
+        for p in parameters:
+            p.grad = None
         loss.backward()
-        assert w1.grad is not None
-        assert b1.grad is not None
-        assert w_out.grad is not None
-        assert b_out.grad is not None
 
-        w1.data += -lr * w1.grad
-        b1.data += -lr * b1.grad
-        w_out.data += -lr * w_out.grad
-        b_out.data += -lr * b_out.grad
+        for p in parameters:
+            assert p.grad is not None
+            p.data += -lr * p.grad
 
 
 def train_mlp():
@@ -75,9 +75,5 @@ def train_mlp():
 
     training = torch.tensor(training)
     validation = torch.tensor(validation)
-    embedding_space = torch.randn(
-        27, embedding_dims
-    )  # Randomly initialize the embeddings, learned via gradient optimization
-    cur_embeddings = embedding_space[training]
 
-    forward_pass(cur_embeddings, validation, num_training_iter)
+    forward_pass(training, validation, num_training_iter)
